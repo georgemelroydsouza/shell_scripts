f3b77db180595903d9e4b9e6381febdf141d6b4e
George Melroy <george@buddi.co.uk>
2024-04-29 16:42:00 +0100

    Shell script to archive older log files

---
+++ archive_older_log_files.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+########################################
+#
+# STEPS
+# 1) Provide the path of the directory
+# 2) Check if the directory is present or not
+# 3) Create 'archive' folder if not already present
+# 4) Find all the files with size more than 20 MB
+# 5) Compress each file
+# 6) Move the compressed files in the 'archive' folder
+# 7) Make a cron job to run the script every day at given time
+#########################################
+
